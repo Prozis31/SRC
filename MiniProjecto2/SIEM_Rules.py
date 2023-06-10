8968aaed128169a6598d18e428940d7a522c8c99
Prozis31 <e.cruz@ua.pt>
2023-06-10 17:53:39 +0100

    Update

--- MiniProjecto2/SIEM_Rules.py
+++ MiniProjecto2/SIEM_Rules.py
@@ -6,24 +6,28 @@ def update_flows(flows, geoip_file, geoipasnum_file):
     # Geolocation of public destination addresses
     gi_country = pygeoip.GeoIP(geoip_file)
     gi_asnum = pygeoip.GeoIP(geoipasnum_file)
-    
+
     # Calculate country code and organization
     flows['country_code'] = flows['dst_ip'].apply(lambda x: gi_country.country_code_by_addr(x) if pd.notnull(x) else None)
     flows['organization'] = flows['dst_ip'].apply(lambda x: gi_asnum.org_by_addr(x) if pd.notnull(x) else None)
-    
-    # Convert timestamp to datetime format
-    flows['timestamp'] = pd.to_datetime(flows['timestamp'], unit='s', origin='unix')
 
-    # Calculate the number of connections in a minute
-    flows['num_connections'] = flows.groupby(pd.Grouper(key='timestamp', freq='1Min')).transform('count')['src_ip']
+    # Convert timestamp to datetime format
+    flows['timestamp'] = pd.to_datetime(flows['timestamp'] * 10**7)  # Convert to nanoseconds (1/100th of a second precision)
+
+    # Sort flows by timestamp
+    flows = flows.sort_values('timestamp')
+
+    # Calculate the number of connections in a minute for each unique combination
+    flows['connections_per_1min'] = flows.groupby(['src_ip', 'dst_ip', pd.Grouper(key='timestamp', freq='1Min')])['src_ip'].transform('count')
 
     # Calculate the ratio of down_bytes to up_bytes for each unique combination
     flows['ratio_down_up_bytes'] = flows.groupby(['src_ip', 'dst_ip']).apply(lambda x: x['down_bytes'].sum() / x['up_bytes'].sum()).reset_index(drop=True)
-    
+
     return flows
 
+
 def filter_src_dst_dst_ip(flows):
-    dst_ip_pattern = r'192\.168\.108\.[0-9]{3}$'
+    dst_ip_pattern = '192.168.108.[0-9]{3}'
 
     filtered_flows = flows[flows['dst_ip'].str.match(dst_ip_pattern) & (flows['dst_ip'] != '192.168.108.240')]
 
@@ -36,8 +40,8 @@ def filter_unwanted_organizations(flows):
     # Filter flows with (down_bytes / up_bytes) < 90
     flows1 = flows[flows['down_bytes'] / flows['up_bytes'] >= 90]
     
-    # Filter flows with low number of connections per 1 min (< 15)
-    flows2 = flows[flows['connections_per_1min'] >= 15]
+    # Filter flows with low number of connections per 1 min (< 30)
+    flows2 = flows[flows['connections_per_1min'] >= 30]
     
     # Merge the two filtered flows
     filtered_flows = pd.concat([flows1, flows2])
@@ -53,7 +57,7 @@ def filter_values_not_present(flows, protocols, country_codes, organizations, de
         ~flows['proto'].isin(protocols) |
         ~flows['country_code'].isin(country_codes) |
         ~flows['organization'].isin(organizations) |
-        ~flows['destination_port'].isin(destination_ports)
+        ~flows['port'].isin(destination_ports)
     ]
     
     return filtered_flows
@@ -87,6 +91,6 @@ flows3 = filter_values_not_present(flows, protocols, country_codes, organization
 filtered_flows = pd.concat([flows1, flows2, flows3]).drop_duplicates()
 
 # Save src_ips to a CSV file
-src_ips = flows['src_ip'].unique()
+src_ips = filtered_flows['src_ip'].unique()
 df_src_ips = pd.DataFrame({'src_ip': src_ips})
-df_src_ips.to_csv('compromised_src_ips.csv', index=False)
+df_src_ips.to_csv('csvs/compromised_src_ips.csv', index=False)
